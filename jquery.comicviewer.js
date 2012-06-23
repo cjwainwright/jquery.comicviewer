2e60a110820ec82f2b9473138a67c8a85a557cf3
Chris Wainwright <cjwainwright@cantab.net>
2012-06-23 17:04:40 +0100

    Adding MIT License; other minor tweaks

--- jquery.comicviewer.js
+++ jquery.comicviewer.js
@@ -1,3 +1,26 @@
+/*!
+Copyright (c) 2012, C J Wainwright, http://cjwainwright.co.uk
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 (function ($) {
     function getRawPanels(panelData) {
         var rawPanels = [];
@@ -101,7 +124,7 @@
                 var paddingFactor = 0.9;
                 scale *= paddingFactor;
                 if(scale > 1) {
-                    scale = Math.floor(scale * 4) / 4; // optimise scaling ratios to for best image quality
+                    scale = Math.floor(scale * 4) / 4; // TODO - optimise scaling ratios to for best image quality
                 }
                                 
                 $viewport.css({
